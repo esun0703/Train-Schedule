7e4c59975b213db890c2134c50736b8fc595a681
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-01-18 20:18:09 -0500

    added database.ref functions, changed var childName, childDestination, childTime, childFrequency, currentTime, convertedChildTime, diffTime, remainder, minUntilTrain, nextTrain, and added respective functions, and line 60.

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -21,14 +21,14 @@ $("#add-train-btn").on("click", function() {
 	var trainName = $("#train-name-input").val().trim();
 	var trainDestination = $("#destination-input").val().trim();
 
-	var firstTrainTime = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
-	var trainFrequency = $("#rate-input").val().trim();
+	var firstTrainTime = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
+	var trainFrequency = $("#frequency-input").val().trim();
 
 	database.ref().push({
-		employeeName:employeeName,
-		employeeRole:employeeRole,
-		employeeStartDate:firebase.database.ServerValue.TIMESTAMP,
-		employeeMonthRate:employeeMonthRate,
+		trainName:trainName,
+		trainDestination:trainDestination,
+		firstTrainTime:firstTrainTime,
+		trainFrequency:trainFrequency,
 	});
 	
 });
@@ -37,25 +37,26 @@ $("#add-train-btn").on("click", function() {
 database.ref().on("child_added", function(childSnapShot){
 			console.log("on child added called");
 			//log everything thats coming out of 
-			console.log(childSnapShot.val().employeeName);
-			console.log(childSnapShot.val().employeeRole);
-			console.log(childSnapShot.val().employeeStartDate);
-			console.log(childSnapShot.val().employeeMonthRate);
+			console.log(childSnapShot.val().trainName);
+			console.log(childSnapShot.val().trainDestination);
+			console.log(childSnapShot.val().firstTrainTime);
+			console.log(childSnapShot.val().trainFrequency);
 
-			var childName = childSnapShot.val().employeeName;
-			var childRole = childSnapShot.val().employeeRole;
-			var childDate = childSnapShot.val().employeeStartDate;
-			var childRate = childSnapShot.val().employeeMonthRate;
-			var childBilled
-
-			var convertedChildDate = moment.unix(childDate).format('MM/DD/YY');
-
-			var empMonths = moment().diff(moment.unix(childDate, "X"), "months");
-
-
-
-			$("tbody").append("<tr>" +
-				"<td>" +childName + "</td><td>" + childRole + "</td><td>" + childDate + "</td><td>" + empMonths + "</td><<td>" + childRate + "</td></tr>");
+			var childName = childSnapShot.val().trainName;
+			var childDestination = childSnapShot.val().trainDestination;
+			var childTime = childSnapShot.val().firstTrainTime;
+			var childFrequency = childSnapShot.val().trainFrequency;
+			
+			var currentTime=moment();
+			var convertedChildTime = moment.unix(childTime).format('hh:mm');
+			var diffTime = moment().diff(moment(convertedChildTime, "X"), "minutes");
+			var remainder = diffTime % childFrequency;
+			//var trainNext = moment().diff(moment.unix(childTime, "X"), "months");
+			var minUntilTrain = childFrequency - remainder;
+			var nextTrain = moment().add(minUntilTrain, "minutes");
 
 
-		});
+
+			$("#train-display").append("<tr><td>" + childName + "</td><td>" + childDestination + "</td><td>" + "Every " + childFrequency + " mins" + "</td><td>" + moment(nextTrain).format("hh:mm A") + "</td><td>" + minUntilTrain + " minutes until arrival" + "</td>");
+
+})
